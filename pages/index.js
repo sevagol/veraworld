3e7b04bfee173339edca2869a57d2395cc85f1b5
sevagol <sevgol1553@gmail.com>
2023-01-05 00:08:58 +0100

    add popup films

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,30 @@ import AddTodo from "../components/AddTodo";
 import Auth from "../components/Auth";
 import TodoList from "../components/TodoList";
 import Link from "next/link";
+import { Button } from "@chakra-ui/react";
+import { Portal } from "@chakra-ui/react";
 import {
     Breadcrumb,
     BreadcrumbItem,
     BreadcrumbLink,
     BreadcrumbSeparator,
   } from '@chakra-ui/react'
+  import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+    PopoverAnchor,
+  } from '@chakra-ui/react'
+import AddFilm from "../components/AddFilm";
+
 export default function Home() {
 return (
+<Container maxW="7xl">
 <Breadcrumb>
   <BreadcrumbItem>
     <BreadcrumbLink href='/todo'>Планы</BreadcrumbLink>
@@ -25,5 +41,20 @@ return (
   </BreadcrumbItem>
   <Auth />
 </Breadcrumb>
+<Popover>
+  <PopoverTrigger>
+    <Button>Добавить кинчик</Button>
+  </PopoverTrigger>
+  <Portal>
+    <PopoverContent>
+      <PopoverArrow />
+      <PopoverCloseButton />
+      <PopoverBody>
+        <AddFilm/>
+      </PopoverBody>
+    </PopoverContent>
+  </Portal>
+</Popover>
+</Container>
 )
 }

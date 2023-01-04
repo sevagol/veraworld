0f3992a9e74b5b6280173dd6cb08881f30a69587
sevagol <sevgol1553@gmail.com>
2023-01-04 16:59:37 +0100

    Move sigh in to homepage

--- pages/todo.js
+++ pages/todo.js
@@ -2,12 +2,11 @@ import { Container } from "@chakra-ui/react";
 import AddTodo from "../components/AddTodo";
 import Auth from "../components/Auth";
 import TodoList from "../components/TodoList";
-import Link from "next/link";
 export default function Home() {
 return (
 <Container maxW="7xl">
-<Auth />
-<Link href="/todo">Планы</Link>
+<AddTodo />
+<TodoList />
 </Container>
 );
 }

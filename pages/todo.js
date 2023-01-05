4e51a352032c54d9ac999d1c833f0503a375c0d1
sevagol <sevgol1553@gmail.com>
2023-01-05 01:07:18 +0100

    finished film and dreams pages

--- pages/todo.js
+++ pages/todo.js
@@ -2,10 +2,36 @@ import { Container } from "@chakra-ui/react";
 import AddTodo from "../components/AddTodo";
 import Auth from "../components/Auth";
 import TodoList from "../components/TodoList";
+import { Button } from "@chakra-ui/react";
+import { Portal } from "@chakra-ui/react";
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+    PopoverAnchor,
+  } from '@chakra-ui/react'
 export default function Home() {
 return (
 <Container maxW="7xl">
-<AddTodo />
+<Popover>
+  <PopoverTrigger>
+    <Button block size="lg" w='100%' >Добавить мечту</Button>
+  </PopoverTrigger>
+  <Portal>
+    <PopoverContent>
+      <PopoverArrow />
+      <PopoverCloseButton />
+      <PopoverBody>
+        <AddTodo/>
+      </PopoverBody>
+    </PopoverContent>
+  </Portal>
+</Popover>
 <TodoList />
 </Container>
 );
